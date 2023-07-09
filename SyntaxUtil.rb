17d918d12467ba5e25f06fb06e5d2d5d3e610264
HelloWorld-n <user@DESKTOP-EHJ1PU1>
2023-07-09 12:51:19 +0200

    ...

--- SyntaxUtil.rb
+++ SyntaxUtil.rb
@@ -3,25 +3,25 @@ class SyntaxUtil
     def self.syntax_string(the_string, the_token)
         if the_token == :variable
             return (
-                "\e[38;2;192;127;64m"\
+                "\e[38;2;253;147;83m"\
                 "#{the_string}"\
                 "\e[0m"\
             )
         elsif the_token == :string
             return (
-                "\e[38;2;184;184;96m"\
+                "\e[38;2;252;229;102m"\
                 "#{the_string}"\
                 "\e[0m"\
             )
         elsif the_token == :int
             return (
-                "\e[38;2;127;96;196m"\
+                "\e[38;2;148;138;227m"\
                 "#{the_string}"\
                 "\e[0m"\
             )
         elsif the_token == :float
             return (
-                "\e[38;2;127;64;127m"\
+                "\e[38;2;180;136;227m"\
                 "#{the_string}"\
                 "\e[0m"\
             )
